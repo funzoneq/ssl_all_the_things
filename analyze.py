db39e349edcf9f9dadb7a5baaec9a832ee8ad891
funzoneq <arnoud@tumblr.com>
2013-01-09 20:58:39 +0100

    More processing, and storing in mongodb

--- analyze.py
+++ analyze.py
@@ -9,7 +9,13 @@ from server.work.models import Certificate
 
 import M2Crypto
 import pprint
+import pymongo
+import simplejson as json
+from datetime import datetime
 
+connection = pymongo.MongoClient()
+db = connection.ssl_all_the_things
+collection = db.certs
 json = {}
 json['ext'] = {}
 
@@ -18,22 +24,30 @@ def split_ext(ext):
 		save = []
 		for k in ext:
 			l = k.split(", ")
-			pprint.pprint(l)
 			for m in l:
 				save.append(m.strip().split(":"))
 		return save
 	except:
 		pass
 
-certs = Certificate.objects.all()[:10]
+certs = Certificate.objects.all()[:60000]
 for x509 in certs:
 	# http://www.heikkitoivonen.net/m2crypto/api/M2Crypto.X509-module.html
 	cert = M2Crypto.X509.load_cert_string(str(x509.pem))
 	#cert = M2Crypto.X509.load_cert('tumblr.pem')
 
 	# http://www.heikkitoivonen.net/m2crypto/api/M2Crypto.X509.X509-class.html
-	json['issuer'] = cert.get_issuer().as_text().split(", ")
-	json['subject'] = cert.get_subject().as_text().split(", ")
+	try:
+		json['issuer'] = cert.get_issuer().as_text()
+		json['subject'] = cert.get_subject().as_text()
+
+		if "," in json['issuer']:
+			json['issuer'] = json['issuer'].split(", ")
+
+		if "," in json['subject']:
+			json['subject'] = json['subject'].split(", ")
+	except:
+		pass
 
 	# http://www.heikkitoivonen.net/m2crypto/api/M2Crypto.X509.X509-class.html#get_fingerprint
 	json['fingerprint'] = cert.get_fingerprint()
@@ -43,13 +57,20 @@ for x509 in certs:
 	json['pub_key'] = cert.get_pubkey().get_rsa().as_pem()
 
 	# http://www.heikkitoivonen.net/m2crypto/api/M2Crypto.ASN1.ASN1_UTCTIME-class.html
-	json['not_before'] = cert.get_not_before().get_datetime()
-	json['not_after'] = cert.get_not_after().get_datetime()
+	try:
+		json['not_before'] = cert.get_not_before().get_datetime()
+		json['not_after'] = cert.get_not_after().get_datetime()
+	except:
+		json['not_before'] = datetime(1970, 1, 1, 0, 0, 0)
+		json['not_after'] = datetime(1970, 1, 1, 0, 0, 0)
 
 	# http://www.heikkitoivonen.net/m2crypto/api/M2Crypto.X509.X509_Extension-class.html
 	count = cert.get_ext_count()
 	for i in range(0, count):
-		value = cert.get_ext_at(i).get_value().strip().split("\n")
+		try:
+			value = cert.get_ext_at(i).get_value().strip().split("\n")
+		except:
+			value = ""
 		key = cert.get_ext_at(i).get_name()
 
 		if key == "subjectAltName" or key == "basicConstraints":
@@ -57,4 +78,10 @@ for x509 in certs:
 
 		json['ext'][key] = value
 
-	pprint.pprint(json)
+	try:
+		cert_id = collection.insert(json)
+		print cert_id, json['subject']
+	except pymongo.errors.DuplicateKeyError:
+		pass
+	except:
+		pass
