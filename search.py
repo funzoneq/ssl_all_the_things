03b006bfaacb465d540d9862998f2a33fc8f97cb
funzoneq <arnoud@tumblr.com>
2013-02-03 18:20:54 +0100

    Searching through the certs

--- search.py
+++ search.py
@@ -1,10 +1,15 @@
-import pymongo
+import pymongo, datetime
 from pprint import pprint
 
 connection = pymongo.MongoClient()
 db = connection.ssl_all_the_things
-collection = db.certs
+certs = db.certs
 
-cert = collection.find_one()
+certs.create_index([ ("pub_key_len", pymongo.ASCENDING), ("subject.CN", pymongo.ASCENDING), ("not_after", pymongo.ASCENDING), ("not_before", pymongo.ASCENDING) ])
 
-pprint (cert)
+today = datetime.datetime.utcnow()
+outofdate = {"not_after": {"$lt": today}}
+weakkeys = {"pub_key_len": {"$lt": 1024}}
+
+for cert in certs.find(outofdate):
+	pprint (cert)
