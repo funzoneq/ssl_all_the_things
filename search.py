276eb30ce41932aa823c80af1061c4360ebbc52d
funzoneq <arnoud@tumblr.com>
2013-02-04 16:26:21 +0100

    Search for issuer GoDaddy

--- search.py
+++ search.py
@@ -5,11 +5,12 @@ connection = pymongo.MongoClient()
 db = connection.ssl_all_the_things
 certs = db.certs
 
-certs.create_index([ ("pub_key_len", pymongo.ASCENDING), ("subject.CN", pymongo.ASCENDING), ("not_after", pymongo.ASCENDING), ("not_before", pymongo.ASCENDING) ])
+certs.create_index([ ("pub_key_len", pymongo.ASCENDING), ("subject.CN", pymongo.ASCENDING), ("not_after", pymongo.ASCENDING) ])
 
 today = datetime.datetime.utcnow()
 outofdate = {"not_after": {"$lt": today}}
 weakkeys = {"pub_key_len": {"$lt": 1024}}
+issuer = {"issuer": "O=GoDaddy.com"}
 
-for cert in certs.find(outofdate):
+for cert in certs.find(issuer):
 	pprint (cert)
